f1de42a1b392c277ca5a2641177f71a978ea7abf
Marcus Sakoda <Jubhub@yahool.com>
2016-12-19 08:52:08 -0800

    initial commit

---
+++ bill.js
@@ -0,0 +1,10 @@
+//write a code that starts with a value.
+//double that value and print it every increment of the loop
+//when you reach 30 days or loop increments, log the final amount
+// if you reach 10,000, log a message
+// how many days until 1 billion?
+// how many days until inifinity?
+
+var reward = 1;
+
+for ()
